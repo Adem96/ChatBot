e8953309f5ce350fc05bc9b2ccfe2b40c8b23620
khalilbenmiled <khalil.benmiled@esprit.tn>
2019-03-30 20:47:10 +0100

    commit specialitePage

--- client/src/component/TousLesSpecialites.js
+++ client/src/component/TousLesSpecialites.js
@@ -5,6 +5,7 @@ import twin from "../images/twin.png";
 import gl from "../images/gl.png";
 import sleam from "../images/sleam.png";
 import ds from "../images/ds.png";
+import arrow from "../images/arrow.png"
 class TousLesSpecialites extends Component {
   constructor() {
     super();
@@ -20,9 +21,35 @@ class TousLesSpecialites extends Component {
 
   render() {
     return (
-      <div className="container">
+      <div className="container-fluid containerBodyS">
         <div className="row rowS">
+        <div className="col-lg-11 colBodyS">
+      
+        <p><span><img className="arrow" src={arrow} alt="arrow"/></span>Spécialités</p>
+        <div className="row">
+        <div className="col-lg-5 containerLogo">
+            <img className="logoOption" src={sim} alt="optionSim" />
+            <button className="buttonOption">Plus d'informations</button>
+          </div>
           <div className="col-lg-5 containerLogo">
+            <img className="logoOption" src={twin} alt="optionSim" />
+            <button className="buttonOption">Plus d'informations</button>
+          </div>
+          <div className="col-lg-5 containerLogo">
+            <img className="logoOption" src={gl} alt="optionSim" />
+            <button className="buttonOption">Plus d'informations</button>
+          </div>
+          <div className="col-lg-5 containerLogo">
+            <img className="logoOption" src={sleam} alt="optionSim" />
+            <button className="buttonOption">Plus d'informations</button>
+          </div>
+          <div className="col-lg-5 containerLogo">
+            <img className="logoOption" src={ds} alt="optionSim" />
+            <button className="buttonOption">Plus d'informations</button>
+          </div>
+</div>
+        </div>
+          {/* <div className="col-lg-5 containerLogo">
             <img className="logoOption" src={sim} alt="optionSim" />
             <button className="buttonOption">Plus d'informations</button>
           </div>
@@ -41,7 +68,9 @@ class TousLesSpecialites extends Component {
           <div className="col-lg-5 containerLogo">
             <img className="logoOption" src={gl} alt="optionSim" />
             <button className="buttonOption">Plus d'informations</button>
-          </div>
+          </div> */}
+
+          
         </div>
       </div>
     );
