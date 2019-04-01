0afa22ca72d9241ebf11b7d2b90abda0e65d8fc9
khalilbenmiled <khalil.benmiled@esprit.tn>
2019-04-01 10:20:49 +0100

    commit details specialite

--- client/src/component/TousLesSpecialites.js
+++ client/src/component/TousLesSpecialites.js
@@ -7,19 +7,45 @@ import sleam from "../images/sleam.png";
 import ds from "../images/ds.png";
 import arrow from "../images/arrow.png"
 import arrowRight from "../images/arrowRight.png"
+import axios from 'axios'
 class TousLesSpecialites extends Component {
   constructor() {
     super();
     this.state = {
-      listSpecialites: []
+      listSpecialites: [],
+      message: ""
     };
   }
+
   componentDidMount() {
     this.setState({
       listSpecialites: this.props.location.state.specialites
     });
   }
-
+  detectIntention(e){
+    e.preventDefault()
+    const msg = {
+      msg : this.state.message
+    }
+    axios.post("http://localhost:4000/options/specialites",msg).then(res => {
+      if(res.data.intent === "Specialite"){
+        this.props.history.push({
+          pathname : '/TousSpecialites',
+          state : { specialites : res.data.response}
+        })
+      }else if(res.data.intent === "detailsSpecialite"){
+        this.props.history.push({
+          pathname : '/DetailsSpecialite',
+          state : { specialite : res.data.response}
+        })
+      }
+    })
+  }
+  onChangeMessage(e){
+    this.setState({
+      message : e.target.value
+    })
+  }
   render() {
     return (
       <div className="container-fluid containerBodyS">
@@ -33,9 +59,10 @@ class TousLesSpecialites extends Component {
                   autoComplete="off"
                   placeholder="Vous avez des questions ?"
                   name="message"
+                  onChange = {this.onChangeMessage.bind(this)}
                   
                 />
-                <img className="arrowRightTS" src={arrowRight} alt="arrowBot"/> 
+                <img className="arrowRightTS" src={arrowRight} alt="arrowBot" onClick={this.detectIntention.bind(this)}/> 
 
         <div className="row">
         <div className="col-lg-5 containerLogo">
