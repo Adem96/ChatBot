99e6aaf9909e0201b0d8efc413bec30249473541
Aymen BEN CHAABANE <37416612+Aymentw@users.noreply.github.com>
2019-04-02 23:41:04 +0200

    Commit

--- controller/scolarite.js
+++ controller/scolarite.js
@@ -1,31 +1,3 @@
 var Subject = require("../models/subject.js");
 
-// function specialites() {
-//
-//
-//   this.getSpecialite = msg => {
-//     return new Promise(function(resolve, reject) {
-//       Specialite.findOne({ nom: msg }, (err, specialite) => {
-//         if (err) reject(err);
-//         else resolve(specialite);
-//       });
-//     });
-//   };
-//   this.getAllSpecialites = () =>{
-//     return new Promise(function(resolve,reject){
-//       Specialite.find((err,specialites)=>{
-//         if(err) reject(err)
-//         else resolve(specialites)
-//       })
-//     })
-//   }
-//   this.getModulesSpecialite = (msg) => {
-//     return new Promise(function(resolve,reject){
-//       Specialite.findOne({nom:msg},(err,moduleS)=>{
-//         if(err) reject(err)
-//         else resolve(moduleS.modulesSpecialite)
-//       })
-//     })
-//   }
-// }
 module.exports = scolarite;
